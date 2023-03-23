078bf11c18e4bedcc6070410a326c88ae26777a8
민재로 <111555810+Minjaero@users.noreply.github.com>
2023-03-23 19:59:13 +0900

    Add files via upload

---
+++ 0323.py
@@ -0,0 +1,28 @@
+#코드업     print("'Hello'")
+#코드업05   print('"Hello World"')
+#코드업06   print("\"!@#$%^&*()'")
+#코드업07   print("\"C:\\Download\\\'hello\'.py\"") 3/13
+#코드업08   print("print(\"Hello\\nWorld\")")       3/14
+# 코드업09 a = input()  3/15
+# 코드업09 print(a)     3/15
+#a = input()
+#a = int(a)     3/17 코드업10
+#print(a)
+#book = int(input("책 값:"))
+#discount = int(input("할인율(%)"))
+#delivery = int(input("배송비"))
+#print("결제하실 금액은" , book / 100 * (100-discount) + delivery , "원 입니다")
+#f = input()    3/19
+#f = float(f)
+#print(f)
+#a = input() 
+#b = input()
+#a=int(a)
+#b=int(b)
+#print(a)
+#print(b)      3/19
+
+a = input()
+b = input()
+print(b)
+print(a)
