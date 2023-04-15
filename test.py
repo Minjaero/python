a8003a44f92b36b6fde3a8eb6c93ee3e33174a62
민재로 <111555810+Minjaero@users.noreply.github.com>
2023-04-15 23:41:06 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -61,3 +61,9 @@
 
 #word1, word2 = input().split()
 #print(word1 + word2)            #4/14
+
+#num1, num2 = input().split()
+#num1 = int(num1)
+#num2 = int(num2)
+
+#print(num1 + num2)                  #4/15
